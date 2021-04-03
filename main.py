86620b78183c976b0d6ecc775799159337e73028
EZLiang <47573436+EZLiang@users.noreply.github.com>
2021-04-03 15:55:13 -0400

    add color

--- main.py
+++ main.py
@@ -14,17 +14,21 @@ async def simon(ctx):
 async def echo(ctx, *, text):
     await ctx.send(text)
 
+
 @alexa.command(name="add")
 async def add(ctx, num1: float, num2: float):
     await ctx.send(num1+num2)
+
 
 @alexa.command(name="subtract")
 async def sub(ctx, num1: float, num2: float):
     await ctx.send(num1-num2)
 
+
 @alexa.command(name="multiply")
 async def mul(ctx, num1: float, num2: float):
     await ctx.send(num1*num2)
+
 
 @alexa.command(name="divide")
 async def div(ctx, num1: float, num2: float):
@@ -32,6 +36,30 @@ async def div(ctx, num1: float, num2: float):
         await ctx.send(num1/num2)
     except ZeroDivisionError:
         await ctx.send("You can't divide by zero!")
+
+
+@alexa.group()
+async def make(ctx):
+    ...
+
+
+@make.command(name="me")
+async def colorize(ctx, col: discord.Colour):
+    rname = "color-" + str(ctx.author.id)
+    role_exists = False
+    for i in ctx.guild.roles:
+        if i.name == rname:
+            role_exists = i
+    if role_exists == False:
+        role_exists = await ctx.guild.create_role(name=rname, color=col)
+    else:
+        await role_exists.edit(color=col)
+    while True:
+        try:
+            await role_exists.edit(position=(role_exists.position+1))
+        except:
+            break
+    await ctx.author.add_roles(role_exists)
 
 
 @alexa.event
