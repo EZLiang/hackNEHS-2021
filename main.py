0cca1dd4ecb28dc6c4facd4bc21fb26e13cea9dc
Ethan <65051086+waitblock@users.noreply.github.com>
2021-04-03 15:31:48 -0400

    Add math commands

--- main.py
+++ main.py
@@ -14,6 +14,25 @@ async def simon(ctx):
 async def echo(ctx, *, text):
     await ctx.send(text)
 
+@alexa.command(name="add")
+async def add(ctx, num1: float, num2: float):
+    await ctx.send(num1+num2)
+
+@alexa.command(name="subtract")
+async def sub(ctx, num1: float, num2: float):
+    await ctx.send(num1-num2)
+
+@alexa.command(name="multiply")
+async def mul(ctx, num1: float, num2: float):
+    await ctx.send(num1*num2)
+
+@alexa.command(name="divide")
+async def div(ctx, num1: float, num2: float):
+    try:
+        await ctx.send(num1/num2)
+    except ZeroDivisionError:
+        await ctx.send("You can't divide by zero!")
+
 
 @alexa.event
 async def on_connect():
