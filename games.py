e640ea9f03580876d77ce365dfefe4f130d60407
EZLiang <47573436+EZLiang@users.noreply.github.com>
2021-04-04 10:03:43 -0400

    fix

--- games.py
+++ games.py
@@ -8,50 +8,47 @@ class BlackJack:
     suits = list("♠♥♣♦")
     ranks = ["A"] + [str(i) for i in range(2, 11)] + ["J", "Q", "K"]
 
-    def __init__(self, ctx, msg=None, reaction=None):
+    def __init__(self, ctx, msg=None):
         self.available_cards = []
         for i in BlackJack.suits:
             for j in BlackJack.ranks:
                 self.available_cards.append([i, j])
+        shuffle(self.available_cards)
         self.win_string = ""
         self.player_hand = []
         self.alexa_hand = []
-        em = discord.Embed()
         self.ctx = ctx
         if msg is None:
-            self.msg = await ctx.send(embed=m)
-            self.new_game()
+            self.msg = None
         else:
-            self.from_message(msg, reaction)
+            self.from_message(msg)
 
     def get_card(self):
-        card = choice(self.available_cards)
-        self.available_cards.remove(card)
-        return card
+        return self.available_cards.pop()
 
-    def new_game(self):
+    async def new_game(self):
+        self.msg = await self.ctx.send(embed=discord.Embed())
         for i in range(2):
             self.player_hand.append(self.get_card())
             self.alexa_hand.append(self.get_card())
-        self.do_player()
+        await self.do_player()
 
-    def from_message(self, message, reaction):
+    def from_message(self, message):
         self.msg = message
-        fields = self.msg.embeds[0].to_dict()
-        player_formatted = fields["Player's hand"].split(" ")
-        alexa_formatted = fields["Alexa's hand"].split(" ")
+        fields = self.msg.embeds[0].to_dict()["fields"]
+        player_formatted = fields[0]["value"].split(" ")
+        alexa_formatted = fields[1]["value"].split(" ")
         self.player_hand = [i for i in map(lambda x: [x[1], x[0]], player_formatted)]
         self.alexa_hand = [i for i in map(lambda x: [x[1], x[0]], alexa_formatted)]
         for i in self.player_hand:
             self.available_cards.remove(i)
         for i in self.alexa_hand:
             self.available_cards.remove(i)
-        self.do_player_response(str(reaction) == "🇦")
 
     def get_hand_sum(self, hand):
         running_sum = 0
-        for i in range(len(hand)):
-            card_rank = (hand[i])[1]
+        for i in hand:
+            card_rank = i[1]
             if card_rank == "A":
                 running_sum += 1
             elif card_rank == "J" or card_rank == "Q" or card_rank == "K":
@@ -66,10 +63,20 @@ class BlackJack:
             return "Busted..."
         if self.get_hand_sum(hand) == 21:
             return "Blackjack!"
+        if self.get_hand_sum(hand) == 11 and self.has_ace(hand):
+            return "Blackjack!"
+        if self.get_hand_sum(hand) < 11 and self.has_ace(hand):
+            return "Current Total: " + str(self.get_hand_sum(hand)) + " or " + str(10 + self.get_hand_sum(hand))
         if self.get_hand_sum(hand) < 21:
-            return "Current Total: " + str(BlackJack.get_hand_sum(hand))
+            return "Current Total: " + str(self.get_hand_sum(hand))
 
-    def update_hands(self):
+    def has_ace(self, hand):
+        for i in hand:
+            if i[1] == "A":
+                return True
+        return False
+
+    async def update_hands(self):
         player_formatted = " ".join([i for i in map(lambda x: x[1] + x[0], self.player_hand)])
         alexa_formatted = " ".join([i for i in map(lambda x: x[1] + x[0], self.alexa_hand)])
         em = discord.Embed()
@@ -77,32 +84,36 @@ class BlackJack:
         em.add_field(name="Alexa's hand", value=alexa_formatted)
         await self.msg.edit(embed=em)
 
-    def do_player(self):
-        self.update_hands()
-        self.msg.edit("🇦 to hit, 🇧 to stand")
+    async def do_player(self):
+        await self.update_hands()
+        await self.msg.edit(content="🇦 to hit, 🇧 to stand")
         await self.msg.add_reaction("🇦")
         await self.msg.add_reaction("🇧")
 
-    def do_player_response(self, hit):
+    async def do_player_response(self, hit):
+        await self.msg.edit(content=None)
+        await self.msg.clear_reactions()
         if hit:
             drawn_card = self.get_card()
             self.player_hand.append(drawn_card)
-            await self.ctx.send("Drawn Card:" + drawn_card[1] + drawn_card[0])
-            self.update_hands()
+            await self.ctx.send("Drawn Card: " + drawn_card[1] + drawn_card[0])
+            await self.update_hands()
+            await self.ctx.send(self.check_hand_valid(self.player_hand))
             if self.check_hand_valid(self.player_hand) == "Busted...":
                 self.win_string = "The computer wins! Better luck next time..."
-                self.endgame()
+                await self.endgame()
             elif self.check_hand_valid(self.player_hand) == "Blackjack!":
                 self.win_string = "You won! Nice job!"
-                self.endgame()
+                await self.endgame()
             else:
-                self.do_player()
+                await sleep(0.5)
+                await self.do_player()
         else:
-            self.handle_alexa()
+            await self.handle_alexa()
 
-    def handle_alexa(self):
-        sleep(0.5)
-        self.update_hands()
+    async def handle_alexa(self):
+        await sleep(0.5)
+        await self.update_hands()
         if self.get_hand_sum(self.alexa_hand) < 17:
             randomizer = randint(1, 12)
             if randomizer == 12:
@@ -120,19 +131,20 @@ class BlackJack:
                 await self.ctx.send("Hit")
                 drawn_card = self.get_card()
                 self.alexa_hand.append(drawn_card)
-                await self.ctx.send("Drawn Card:" + drawn_card[1] + drawn_card[0])
+                await self.ctx.send("Drawn Card: " + drawn_card[1] + drawn_card[0])
+        await self.ctx.send(self.check_hand_valid(self.alexa_hand))
         if self.check_hand_valid(self.alexa_hand) == "Busted...":
             self.win_string = "The player wins! The computer should get smarter..."
-            self.endgame()
+            await self.endgame()
         elif self.check_hand_valid(self.alexa_hand) == "Blackjack!":
             self.win_string = "The computer won! Better luck next time..."
-            self.endgame()
+            await self.endgame()
         else:
-            self.handle_alexa()
+            await self.handle_alexa()
 
-    def endgame(self):
-        self.update_hands()
-        sleep(0.5)
+    async def endgame(self):
+        await self.update_hands()
+        await sleep(0.5)
         await self.ctx.send(self.win_string)
 
 
@@ -144,10 +156,18 @@ class Games(commands.Cog):
         self.player_hand = []
         self.alexa_hand = []
 
-    @commands.command(name="blackjack")
-    async def bj(self, ctx):
-        BlackJack(ctx)
+    @commands.group(name="play")
+    async def play(self, ctx):
+        ...
 
-    @commands.Cog.listener
-    async def on_reaction_add(self, reaction):
-        BlackJack(reaction.message.channel, reaction.message, reaction.emoji)
+    @play.command(name="blackjack")
+    async def bj(self, ctx):
+        foo = BlackJack(ctx)
+        await foo.new_game()
+
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        if user.id == self.bot.user.id:
+            return
+        foo = BlackJack(reaction.message.channel, reaction.message)
+        await foo.do_player_response(str(reaction) == "🇦")
